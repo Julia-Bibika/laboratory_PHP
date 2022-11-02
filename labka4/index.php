de6d21c71563d26f51e1e27d79d2e5e14e59c63a
Julia-Bibika <93942521+Julia-Bibika@users.noreply.github.com>
2022-11-02 20:36:53 +0200

    Update index.php

--- labka4/index.php
+++ labka4/index.php
@@ -4,7 +4,8 @@ use Model\Car;
 use Model\Car\Collection;
 use Model\Car\Repository;
 
-$dbh = new PDO('mysql:host=localhost;dbname=cars_db', 'root', '');
+$dbh = new PDO('mysql:host=127.0.0.1;dbname=cars_db', 'root', '');
+$sql = "SELECT * FROM cars;";
 function myAutoloader($class_name)
 {
     if (!class_exists($class_name)) {
@@ -12,10 +13,20 @@ function myAutoloader($class_name)
     }
 }
 // заповнення бази даних
-function InsertDB($carCollections,$dbh){
+function insertInDb($carCollections,$dbh){
     foreach ($carCollections as $car){
-        $dbh->query('INSERT into сars(id, PIB, mark, number_car,color) values($car->id, $car->PIB, $car->mark,$car->number_car,$car->color)');
+        $sql = 'INSERT INTO сars(id, PIB, mark, number_car,color) VALUES ($car.getId(),$car.getPIB(),$car.getMark(),$car.getNumber_car(),$car.getColor())';
+        $statement = $dbh->prepare($sql);
+        $statement->execute([
+                            $car->id,
+                            $car->PIB,
+                            $car->mark,
+                            $car->number_car,
+                            $car->color
+                            ]);
+        $result = $statement->fetchAll();
     }
+    return $result;
 }
 
 spl_autoload_register('myAutoloader');
@@ -28,6 +39,7 @@ $carCollections = new Collection([$car1,$car2,$car3]);
 $saveCarCollection = new Repository();
 $saveCarCollection->createNewFile('cars');
 $saveCarCollection->storeDataToFile($carCollections, 'cars');
+var_dump(insertInDb($carCollections,$sql));
 var_dump($carCollections);
 echo '<br> ------------------ <br>';
 
